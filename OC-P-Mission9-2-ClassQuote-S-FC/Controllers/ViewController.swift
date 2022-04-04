5ddae6cd3f961f62e014b4f770c423f8b64c218f
AdPiscinam <lab@walimaloui.com>
2022-04-04 10:33:11 +0200

    Add ImageView

--- OC-P-Mission9-2-ClassQuote-S-FC/Controllers/ViewController.swift
+++ OC-P-Mission9-2-ClassQuote-S-FC/Controllers/ViewController.swift
@@ -6,12 +6,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    // UIImageView
+    private let picture: UIImageView = {
+        let imageView = UIImageView()
+        let image = UIImage(named: "sunset.jpeg")
+        imageView.image = image
+        imageView.contentMode = .scaleToFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    
+    // UILabel: Quote
+    // UILabel: Author
+    // UIButton
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupUI()
     }
 
 
+}
+
+//MARK: User Interface Methods
+extension ViewController {
+    
+    private func setupUI() {
+        // Background Color
+        view.backgroundColor = UIColor.backgroundIsabelline
+        view.addSubview(picture)
+        
+        picture.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
+        picture.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
+        picture.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
+        picture.heightAnchor.constraint(equalTo: picture.widthAnchor, multiplier: 1).isActive = true
+        
+        // UIImageView
+        // UILabel: Quote
+        // UILabel: Author
+        // UIButton
+    }
 }
 

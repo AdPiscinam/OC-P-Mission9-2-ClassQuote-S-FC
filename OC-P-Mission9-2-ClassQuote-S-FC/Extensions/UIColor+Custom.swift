f2985a6a94dc9262839fdc98727d95700ea0a6fb
AdPiscinam <lab@walimaloui.com>
2022-04-04 09:45:27 +0200

    Initial Commit

---
+++ OC-P-Mission9-2-ClassQuote-S-FC/Extensions/UIColor+Custom.swift
@@ -0,0 +1,23 @@
+// UIColor+Custom.swift
+// OC-P-Mission9-2-ClassQuote-S-FC
+// Created by Ad Piscinam on 04/04/2022
+// 
+
+import UIKit
+
+extension UIColor {
+    
+    public class var backgroundIsabelline: UIColor {
+        UIColor(red: 0.95, green: 0.93, blue: 0.93, alpha: 1.00)
+    }
+    
+    public class var textViewGray: UIColor {
+        UIColor(red: 0.80, green: 0.80, blue: 0.80, alpha: 1.00)
+    }
+    
+    public class var merlotRed: UIColor {
+        UIColor(red: 0.50, green: 0.12, blue: 0.13, alpha: 1.00)
+    }
+    
+    
+}

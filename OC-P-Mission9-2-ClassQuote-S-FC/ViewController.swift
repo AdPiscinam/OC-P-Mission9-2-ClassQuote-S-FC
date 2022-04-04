88bd65df48f6cfd02047f16c1b7c8f35bd226402
AdPiscinam <lab@walimaloui.com>
2022-04-04 09:33:07 +0200

    Initial Commit

---
+++ OC-P-Mission9-2-ClassQuote-S-FC/ViewController.swift
@@ -0,0 +1,17 @@
+// ViewController.swift
+// OC-P-Mission9-2-ClassQuote-S-FC
+// Created by Ad Piscinam on 04/04/2022
+// 
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

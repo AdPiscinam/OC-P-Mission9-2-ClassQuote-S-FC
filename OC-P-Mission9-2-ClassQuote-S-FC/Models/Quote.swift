acbcda932b0d244e1d307011d86109874be8fd15
AdPiscinam <lab@walimaloui.com>
2022-04-05 07:53:37 +0200

    Create Quote Structure in Models

---
+++ OC-P-Mission9-2-ClassQuote-S-FC/Models/Quote.swift
@@ -0,0 +1,12 @@
+// Quote.swift
+// OC-P-Mission9-2-ClassQuote-S-FC
+// Created by Ad Piscinam on 04/04/2022
+// 
+
+import Foundation
+
+struct Quote {
+    var text: String
+    var author: String
+    var imageData: Data
+}
